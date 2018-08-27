b0e54295e7e79e541b6b652ea4d8e57c2eca9419
kevinten10 <596823919@qq.com>
2018-08-27 16:54:52 +0800

    jdk

---
+++ jdk/src/jdk/Test.java
@@ -0,0 +1,4 @@
+package jdk;
+
+public class Test {
+}
---
+++ jdk/src/jdk/collections/ListReader.java
@@ -0,0 +1,20 @@
+package jdk.collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListReader {
+
+    public static void arrayList() {
+        List list = new ArrayList();
+    }
+
+    public static void linkedList() {
+        List list = new LinkedList();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
